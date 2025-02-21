b264900e1a90412d06dfd6119d91c16ffa63be78
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-21 10:49:06 +0000

    const and var learn

---
+++ Basic-01/const-var.js
@@ -0,0 +1,11 @@
+const accountId = 19383
+let accountEmail = "parasmoguls@gmail.com"
+var accountPassword = "ParasMeena"
+//accountCity = "Delhi"
+
+// accountId = 77656564
+accountEmail = "parasmeena@gmail.com"
+accountPassword = "76tca7iayuiudgas" 
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
