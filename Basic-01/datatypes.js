995aef68cf5ea1cc89bffb34f5de7240c7edb63a
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-21 11:46:27 +0000

    dataTypes discuss

---
+++ Basic-01/datatypes.js
@@ -0,0 +1,11 @@
+// number ( 0987654321 )
+// string  ( qwertyuiolkjhgfdsxcvbnm )
+// boolean ( False and True )
+// bigint ( Big number )   
+// null  ( standalone value = Blank) - type Object
+// undefined ( not define value )
+// symbol ( unique )
+
+// object (  )
+
+console.log(typeof 4567);
