ddfe432fe992cea5368fb39f76033e4a565e99ac
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-21 12:21:32 +0000

    Conversion Type

---
+++ Basic-01/datatype-Conversion.js
@@ -0,0 +1,27 @@
+// let score = 'paras'
+
+
+// console.log(typeof score); 
+// console.log(typeof (score)); 
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+
+// string convert to number easyly "33" to 33
+// string number not convert  NaN "5345hkkds" to NaN
+// Boolean true = 1 and false = 0
+// empty value = false
+// with value = true
+
+// let isLoggedIn = ""
+// let isLoggedIn_boolean =  Boolean(isLoggedIn);
+
+// console.log(isLoggedIn_boolean);
+
+let NameS = null;
+let NameString = Boolean(NameS)
+
+console.log(NameString);
+console.log(typeof NameString);
