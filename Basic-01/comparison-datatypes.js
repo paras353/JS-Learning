1f7c20b60dff614140681580632f77a63966d7c6
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-24 07:34:32 +0000

    comparision data type and data types summary

---
+++ Basic-01/comparison-datatypes.js
@@ -0,0 +1,7 @@
+console.log(2>4);
+console.log(null>"0");
+console.log(undefined>"0");
+console.log(null == "0");
+console.log(undefined === "0");
+console.log("0" == "0");
+console.log(undefined === "0");
---
+++ Basic-01/datatypes-summary.js
@@ -0,0 +1,9 @@
+// Primitive Types
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+
+// Reference ( Non Primitive )
+// Array, Objects, Functions
