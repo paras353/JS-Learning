b71a45f246abdb2d26f52f7627ca1d3cecee891b
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-24 08:03:01 +0000

    Data Type Summary

--- Basic-01/datatypes-summary.js
+++ Basic-01/datatypes-summary.js
@@ -2,8 +2,33 @@
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 
+// const testId = Symbol('123');
+// console.log(testId);
+
+// const testBigIntNum = 98765432564766756575764564353754351234567n;
+// console.log(typeof testBigIntNum);
+
+// const testI = 987654325;
+// console.log(typeof testI);
+
 
 
 
 // Reference ( Non Primitive )
 // Array, Objects, Functions
+
+
+// const testArray = ["Thor", "IronMen", "Black Panther"]
+// console.log(testArray);
+
+// const myObjTest = {
+//     name: "TestName",
+//     Age: 30
+// }
+
+// console.log(myObjTest);
+
+const myFun = function() { 
+    console.log("Hellow World");
+}
+console.log(typeof myFun);
