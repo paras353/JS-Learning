d1a7aea177d370bdf183cdeeb1d8f59d9fc62ca4
paras353 <117708471+paras353@users.noreply.github.com>
2025-02-24 12:51:44 +0000

    num & Math

---
+++ Basic-01/number-maths.js
@@ -0,0 +1,25 @@
+const score = 1000000000
+// console.log(score);
+
+//const balance = new Number(1000);
+// console.log(balance.toFixed(1)); 
+// console.log(balance.toString().length); 
+// console.log(score.toPrecision(4)); 
+// console.log(score.toLocaleString('en-IN')); 
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(3423.4234));
+// console.log(Math.ceil(1.3));
+// console.log(Math.floor(7.9));
+// console.log(Math.sqrt(100));
+// console.log(Math.min(3,5,8,9));
+// console.log(Math.max(4534,4353453,23567,78978979));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
+// console.log((Math.random()+10) +1 );
